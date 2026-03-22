a7e21b62ed81cc8776811c90b61bd122038b887e
Rishi R <rishirreddy18@gmail.com>
2026-03-22 11:45:58 +0000

    Binary search

---
+++ BinarySearch.java
@@ -0,0 +1,19 @@
+//https://www.geeksforgeeks.org/problems/binary-search-1587115620/1?company=
+class Solution {
+    public int binarysearch(int[] arr, int k) {
+        // Code Here
+        int left = 0;
+        int right= arr.length-1;
+        while(left<right){
+            int mid = (left+right)/2;
+            if(arr[mid] == k){
+                return mid;
+            }else if(arr[mid]<k){
+                left = mid+1;
+            }else{
+                right = mid-1;
+            }
+        }
+        return -1;
+    }
+}
